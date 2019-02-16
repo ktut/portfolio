bc6f40bc1a1ac92370dfeb257756a9c5a79037b1
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-02-16 17:23:53 -0600

    many more changes to fix logic and add point system

--- clubgame.js
+++ clubgame.js
@@ -28,6 +28,8 @@ $(document).ready(function() {
         let score = 0;
         let money = 1200;
         let alcohol = 0;
+        let roomWidth = parseFloat($(".room").width());
+        let roomHeight = parseFloat($(".room").height());
 
         //populate fields
         $(".money").text(money);
@@ -39,30 +41,46 @@ $(document).ready(function() {
         // things to do per second
         setInterval(function () {
             time++;
+            let pointsString = "";
+            numGirls = $("#seat").children(".girl").length;
+            numGuys = $("#seat").children(".guy").length;
             
             // at table, girls = two points, guys = negative one point
-            score += 2 * $("#seat").children(".girl").length;
-            score -= $("#seat").children(".guy").length;
+            score += 2 * numGirls;
+            score -= numGuys;
 
+            // add these in points string
+            pointsString += "+2 ".repeat(numGirls);
+            pointsString += "-1 ".repeat(numGuys);
+
+            // show pointsstring, show score
+            $("#points").text(pointsString);
             $(".score").text(score);
 
             // sometimes, if low alcohol, and people at table, they leave
-            if (Math.random() < .2 && alcohol < 30 && $("#seat").children().length) {
-                var xe = -1 * $("#seat").offset().left;
-                var ye = -1 * $("#seat").offset().top;
+            if (Math.random() < .1 && alcohol < 30 && $("#seat").children().length) {
 
                 let booted = $("#seat .person:last-child");
+                let xe = booted.offset.left;
+                let ye = booted.offset.top;
+                let newx = roomWidth * Math.random();
+                let newy = roomHeight * Math.random();
+                booted.removeClass("selected");
 
                 booted.animate({
                     top: ye,
                     left: xe     
                 }, 550, function() {
                     $(".room").append(booted);
+                    booted.css({
+                        top: newy, 
+                        left: newx
+                    });
                 });
             }
 
             // girls flock to bottle
-            if (Math.random() < .4 && bottle) {
+            if (Math.random() < .3 && bottle && $("#seat").children().length <= 10) {
                 
                 var xi = $("#seat").offset().left;
                 var yi = $("#seat").offset().top;
@@ -73,7 +91,7 @@ $(document).ready(function() {
                 bottleRat.animate({
                     top: yi,
                     left: xi     
-                }, 750, function() {
+                }, 1000, function() {
                     $("#seat").append(bottleRat);
                 });
             }
@@ -87,11 +105,9 @@ $(document).ready(function() {
                 $(".bottle").fadeOut();
             }
 
-
-
             //spawn person
-            // $('.room').append($('<div class="person">'));
-        }, 1000); 
+            // $('.room').append($('<div class="person girl">'));
+        }, 200); 
         
         
         // people random movement logic
@@ -115,13 +131,27 @@ $(document).ready(function() {
         };
 
         //make people draggable
-        $( ".person" ).draggable();
+        $( ".person" ).draggable({
+            cursor: 'move',
+            stack: "div",
+            distance: 0
+        });
 
         // make seat droppable
-        $( "#seat" ).droppable({
+        $( ".table" ).droppable({
             drop: function( event, ui ) {
             $( this )
-                $("#seat").append($(ui.draggable));
+                $("#seat").append($(ui.draggable))
+                $(ui.draggable).addClass("selected");
+            }
+        });
+
+        // make room also droppable
+        $( ".room" ).droppable({
+            drop: function( event, ui ) {
+            $( this )
+                $(this).append($(ui.draggable))
+                $(ui.draggable).removeClass("selected");
             }
         });
 
