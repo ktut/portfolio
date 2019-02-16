a60795e7a3f48b2a3408ff9ea8f58f936908b1ba
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-02-15 18:32:43 -0600

    test hack to scroll past address bar in mobile safari

--- clubgame.js
+++ clubgame.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	}, 0);
+
     // declare initial global variables
     let numBottles = 0;
     let numGirls = 0;
