29246c5e68679173c5af090965c61b6fdfb79a22
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-03-25 19:59:40 -0500

    added major updatee with video for design piece previews

--- main.js
+++ main.js
@@ -7,31 +7,43 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 500);
 });
 
+// video play on hover
+$( document ).ready(function() {
+    $(".web-img video").fadeIn(1000);
+    $(".web-img").hover(function () {
+        $(this).children("video")[0].play();
+    }, function () {
+        var el = $(this).children("video")[0];
+        el.pause();
+    });
+});
+
+
 
 // create slider
-var priceRange = document.getElementById('range');
+// var priceRange = document.getElementById('range');
 
-noUiSlider.create(priceRange, {
-    start: [ 10 ],
-    step: 10,
-    range: {
-        'min': 0,
-        'max': 20
-    }
-});
+// noUiSlider.create(priceRange, {
+//     start: [ 10 ],
+//     step: 10,
+//     range: {
+//         'min': 0,
+//         'max': 20
+//     }
+// });
 
-// When the slider value changes, update the span and div
-priceRange.noUiSlider.on('update', function( values, handle ) {
-    if (values[handle] == 0.00) {
-        jQuery("body").addClass("no-ui");
-        jQuery("body").removeClass("extra-ui");
-    } else if (values[handle] == 10.00) {
-        jQuery("body").removeClass("no-ui");
-    } else if (values[handle] == 20.00) {
-        jQuery("body").addClass("extra-ui");
-        jQuery("body").removeClass("no-ui");
-    }
-});
+// // When the slider value changes, update the span and div
+// priceRange.noUiSlider.on('update', function( values, handle ) {
+//     if (values[handle] == 0.00) {
+//         jQuery("body").addClass("no-ui");
+//         jQuery("body").removeClass("extra-ui");
+//     } else if (values[handle] == 10.00) {
+//         jQuery("body").removeClass("no-ui");
+//     } else if (values[handle] == 20.00) {
+//         jQuery("body").addClass("extra-ui");
+//         jQuery("body").removeClass("no-ui");
+//     }
+// });
 
 // on topper image click, scroll down to slider, move slider and sections appropriately (using callback function)
 // jQuery( ".real-topper > a" ).click(function() {
